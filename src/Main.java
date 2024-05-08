e272c1dda8a528e7031b8cf2e8630cfa0ee61a0b
Андрей <andrej.chugunov2002@yandex.ru>
2024-05-08 15:01:48 +0300

    Доработка ДЗ

--- src/Main.java
+++ src/Main.java
@@ -8,53 +8,46 @@ public class Main {
             for (int i = 0; i < payments.length; i++) {
                 sum += payments[i];
                 if (i == payments.length - 1) {
-                    System.out.println(" Сумма трат за меясц составила " + sum + " рублей");
+                    System.out.println(" Сумма трат за месяц составила " + sum + " рублей");
                 }
             }
         }
         System.out.println("Задание 2");
         {
             int[] payments = {100000, 150000, 130000, 126700, 113900};
-            int minPayment = 0;
-            int maxPayment = 0;
+            int minPayment = payments[1];
+            int maxPayment = Integer.MIN_VALUE;
             for (final int current : payments) {
                 if (current > maxPayment) {
                     maxPayment = current;
                 }
-                for (final int current2 : payments) {
-                    if (current2 < current) {
-                        minPayment = current2;
-                    }
+                if (minPayment > current) {
+                    minPayment = current;
                 }
             }
             System.out.println(" Минимальная сумма трат за неделю составила " + minPayment +
                     ". Максимальная сумма трат за неделю составила " + maxPayment + " рублей");
         }
         System.out.println("Задание 3");
+
         {
             int[] payments = {100000, 150000, 130000, 126700, 113999};
             double sum = 0;
-            int avg;
-            double avg1;
+            double avg;
             for (int i = 0; i < payments.length; i++) {
                 sum += payments[i];
                 if (i == payments.length - 1) {
-                    if (sum % payments.length == 0) {
-                        avg = (int) (sum / payments.length);
+                        avg = (sum / payments.length);
                         System.out.println(" Средняя сумма трат за месяц составила " + avg + " рублей");
-                    } else {
-                        avg1 = sum / payments.length;
-                        System.out.println(" Средняя сумма трат за месяц составила " + avg1 + " рублей");
                     }
                 }
             }
-        }
-        System.out.println("Задание 4");
-        {
-            char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-            for (int i = reverseFullName.length - 1; i < reverseFullName.length && i > -1; i--) {
-                System.out.print(reverseFullName[i]);
+            System.out.println("Задание 4");
+            {
+                char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+                for (int i = reverseFullName.length - 1; i < reverseFullName.length && i > -1; i--) {
+                    System.out.print(reverseFullName[i]);
+                }
             }
         }
     }
-}
